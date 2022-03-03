9600ac2553da01ba50b201a0b87766447a82aa66
uf-code <ugur.firat.developer@gmail.com>
2022-03-03 14:11:21 +0300

    udp socket server and client examples

---
+++ 02-udp_sockets/02_udp_socket_client.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
+func main() {
+	connection_host := "localhost"
+	connection_port := "8080"
+
+	connection_addr := connection_host + ":" + connection_port
+
+	RemoteAddr, err := net.ResolveUDPAddr("udp", connection_addr)
+
+	//LocalAddr := nil
+	// see https://golang.org/pkg/net/#DialUDP
+
+	conn, err := net.DialUDP("udp", nil, RemoteAddr)
+
+	// note : you can use net.ResolveUDPAddr for LocalAddr as well
+	//        for this tutorial simplicity sake, we will just use nil
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Established connection to %s \n", connection_addr)
+	log.Printf("Remote UDP address : %s \n", conn.RemoteAddr().String())
+	log.Printf("Local UDP client address : %s \n", conn.LocalAddr().String())
+
+	defer conn.Close()
+
+	// write a message to server
+	message := []byte("Go is Fun!")
+
+	_, err = conn.Write(message)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	// receive message from server
+	buffer := make([]byte, 1024)
+	n, addr, err := conn.ReadFromUDP(buffer)
+
+	fmt.Println("UDP Server : ", addr)
+	fmt.Println("Received from UDP server : ", string(buffer[:n]))
+
+}
