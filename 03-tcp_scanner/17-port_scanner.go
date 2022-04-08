4a06ad3f109e34148bd6aed9fe67dd7a80f3929d
uf-code <ugur.firat.developer@gmail.com>
2022-04-08 14:33:53 +0300

    changing few things

---
+++ 03-tcp_scanner/17-port_scanner.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"strconv"
+	"time"
+)
+
+type ScanResult struct {
+	Port    string
+	State   string
+	Service string
+}
+
+func ScanPort(protocol, hostname string, port int) ScanResult {
+	result := ScanResult{Port: strconv.Itoa(port) + string("/") + protocol}
+	address := hostname + ":" + strconv.Itoa(port)
+	conn, err := net.DialTimeout(protocol, address, 60*time.Second)
+
+	if err != nil {
+		result.State = "Closed"
+		return result
+	}
+	defer conn.Close()
+	result.State = "Open"
+	return result
+}
+
+func InitialScan(hostname string) []ScanResult {
+
+	var results []ScanResult
+
+	for i := 0; i <= 1024; i++ {
+		results = append(results, ScanPort("udp", hostname, i))
+		fmt.Printf("Scanning UDP Port %v\n", i)
+	}
+
+	for i := 0; i <= 1024; i++ {
+		results = append(results, ScanPort("tcp", hostname, i))
+		fmt.Printf("Scanning TCP Port %v\n", i)
+
+	}
+
+	return results
+}
+
+func WideScan(hostname string) []ScanResult {
+	var results []ScanResult
+
+	for i := 0; i <= 49152; i++ {
+		results = append(results, ScanPort("udp", hostname, i))
+	}
+
+	for i := 0; i <= 49152; i++ {
+		results = append(results, ScanPort("tcp", hostname, i))
+	}
+
+	return results
+}
+
+func main() {
+
+	quickScan := InitialScan("localhost")
+	fmt.Println(quickScan)
+
+	// scanAll := WideScan("localhost")
+	// fmt.Println(scanAll)
+}
